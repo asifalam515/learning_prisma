0677f734b8a404d00f47f26564c23390eb4bf6ab
asifalam515 <asibulalam515@gmail.com>
2025-12-29 13:33:23 +0600

    user model created by better auth

--- blog_app_prisma/src/app.ts
+++ blog_app_prisma/src/app.ts
@@ -1,8 +1,20 @@
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 import express, { Application } from "express";
+import { auth } from "./lib/auth";
 import { postRouter } from "./modules/post/post.router";
+
 const app: Application = express();
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(express.json());
 app.use("/posts", postRouter);
+app.use(
+  cors({
+    origin: "http://localhost:3000/", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
 app.get("/", (req, res) => {
   res.send("hello world");
   console.log("first application running");
