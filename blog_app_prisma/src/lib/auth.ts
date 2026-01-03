e5671299ff3410636d621c85c0c3e74e5e5c03f7
asifalam515 <asibulalam515@gmail.com>
2026-01-03 11:49:56 +0600

    email sending applied

--- blog_app_prisma/src/lib/auth.ts
+++ blog_app_prisma/src/lib/auth.ts
@@ -1,11 +1,38 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+// For production, replace with your actual SMTP server details.
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASSWORD,
+  },
+});
 export const auth = betterAuth({
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      const info = await transporter.sendMail({
+        from: '"Prisma Blog" <prismablog@gmail.com>',
+        to: "asibulalam515@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?", // Plain-text version of the message
+        html: "<b>Hello world?</b>", // HTML version of the message
+      });
+
+      console.log("Message sent:", info.messageId);
+    },
+  },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
   },
+
   user: {
     additionalFields: {
       role: {
