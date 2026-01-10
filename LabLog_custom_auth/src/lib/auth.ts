d7e21e31ef433f4bfbbd1f0914cca6c5e64f053c
asifalam515 <asibulalam515@gmail.com>
2026-01-11 01:55:32 +0600

    two factory applied

--- LabLog_custom_auth/src/lib/auth.ts
+++ LabLog_custom_auth/src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { twoFactor } from "better-auth/plugins";
 import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
@@ -7,6 +8,8 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  appName: "Lab Log",
+
   trustedOrigins: [process.env.FRONTEND_URL!],
   emailAndPassword: {
     enabled: true,
@@ -19,4 +22,5 @@ export const auth = betterAuth({
       redirectURI: `${process.env.FRONTEND_URL}/api/auth/callback/github`,
     },
   },
+  plugins: [twoFactor()],
 });
