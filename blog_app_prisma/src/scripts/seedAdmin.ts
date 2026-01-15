177a20d822c748882b6ed3993fe101be6687285a
asifalam515 <asibulalam515@gmail.com>
2026-01-15 21:06:36 +0600

    admin seeding applied

--- blog_app_prisma/src/scripts/seedAdmin.ts
+++ blog_app_prisma/src/scripts/seedAdmin.ts
@@ -3,18 +3,21 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
   try {
+    console.log("********* admin seeding started!!!...");
     const adminData = {
-      name: "Admin Saheb",
-      email: "admin@admin.com",
+      name: "Admin1 Saheb",
+      email: "admin1@admin.com",
       role: UserRole.ADMIN,
       password: "admin1234",
     };
+    console.log("********Checking admin exists or not");
     // we will check if the user is there in DB
     const existingUser = await prisma.user.findUnique({
       where: {
         email: adminData.email,
       },
     });
+
     if (existingUser) {
       throw new Error("User already Exists in DB!!!");
     }
@@ -25,9 +28,25 @@ async function seedAdmin() {
         headers: {
           "Content-Type": "application/json",
         },
+        body: JSON.stringify(adminData),
       }
     );
+    // admin created now  we  are going to update
+    console.log("Admin created");
+    if (signUpAdmin.ok) {
+      await prisma.user.update({
+        where: {
+          email: adminData.email,
+        },
+        data: {
+          emailVerified: true,
+        },
+      });
+      console.log("###  email verified");
+    }
+    console.log("** success");
   } catch (error) {
     console.error(error);
   }
 }
+seedAdmin();
